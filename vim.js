8ce34b0d584891f0afae59e26fbe36be43e7c807
Tony Majestro <tonymajestro@gmail.com>
2015-05-25 17:28:35 -0700

    Add second line to vim.js

--- vim.js
+++ vim.js
@@ -4,7 +4,7 @@ function Vim() {
     this.y = 0;
     this.text = [
         "Welcome to VimTutor.",
-        "",
+        "Second Line",
         "Press the h, j, k, and l keys to start moving around."];
 
     this.keyHandler = new KeyHandler();
